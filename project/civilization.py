12340c6931d7caf3488612f0e642b16d10c97e29
Vladyslav Tomashpolskyi <wizzardich@gmail.com>
2016-04-02 21:53:52 +0200

    [project] Added a contact penalty to a civ [site] Almost done with Conclusion

--- project/civilization.py
+++ project/civilization.py
@@ -7,6 +7,7 @@ CIV_BENEVOLENT_ATTITUDE = 1
 CIV_MALEVOLENT_TRIGGER_HAPPY = 0.9
 CIV_BENEVOLENT_TALKATIVE = 0.9
 CIV_SURPRISE_BONUS = 2
+CIV_CONTACT_PENALTY = 2
 CIV_DETECTABILITY = 0.5
 
 
@@ -178,6 +179,7 @@ class BenevolentCivilization(Civilization):
 
     def cooperate(self):
         self.state = "contact"
+        self.tech -= CIV_CONTACT_PENALTY
         self.other.contacted()
         return True
 
@@ -248,6 +250,7 @@ class PlayerCivilization(Civilization):
 
     def cooperate(self):
         self.state = "contact"
+        self.tech -= CIV_CONTACT_PENALTY
         print("We will contact other civilization immediately. Keep your fingers crossed so they don't destroy us.")
         self.other.contacted()
         return True
